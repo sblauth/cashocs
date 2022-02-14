5ac9a282ddcab1fa28b337714e5fce3fe2a966d6
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2022-02-14 08:14:08 +0100

    version bump

--- cashocs/__init__.py
+++ cashocs/__init__.py
@@ -56,7 +56,7 @@ from cashocs.utils import (
 )
 
 
-__version__ = "1.5.9"
+__version__ = "1.5.10"
 
 __all__ = [
     "import_mesh",
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020-2022, Sebastian Blauth"
 author = "Sebastian Blauth"
 
 # The full version, including alpha/beta/rc tags
-release = "1.5.9"
+release = "1.5.10"
 
 
 # -- General configuration ---------------------------------------------------
