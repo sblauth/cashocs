fccc971d78920b7b3d302374a03712d65d8c540b
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2022-07-28 10:50:58 +0200

    version bump

--- cashocs/__init__.py
+++ cashocs/__init__.py
@@ -57,7 +57,7 @@ from cashocs.nonlinear_solvers import damped_newton_solve
 from cashocs.nonlinear_solvers import newton_solve
 from cashocs.nonlinear_solvers import picard_iteration
 
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 
 __citation__ = """
 @Article{Blauth2021cashocs,
