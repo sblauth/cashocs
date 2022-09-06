17ceffa9428f2fbbe7f2e3db6863559ffbcdfa4a
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2022-09-06 09:26:02 +0200

    updated test_log_level for thread safety

--- tests/test_log_level.py
+++ tests/test_log_level.py
@@ -27,76 +27,77 @@ from cashocs._loggers import warning
 
 def test_set_log_level(caplog):
     cashocs.set_log_level(cashocs.LogLevel.DEBUG)
+    debug("abc")
+    info("def")
+    warning("ghi")
+    error("jkl")
+    critical("mno")
     if fenics.MPI.rank(fenics.MPI.comm_world) == 0:
-        debug("abc")
-        info("def")
-        warning("ghi")
-        error("jkl")
-        critical("mno")
         assert "abc" in caplog.text
         assert "def" in caplog.text
         assert "ghi" in caplog.text
         assert "jkl" in caplog.text
         assert "mno" in caplog.text
-        caplog.clear()
     fenics.MPI.barrier(fenics.MPI.comm_world)
+    caplog.clear()
 
     cashocs.set_log_level(cashocs.LogLevel.INFO)
+
+    debug("abc")
+    info("def")
+    warning("ghi")
+    error("jkl")
+    critical("mno")
     if fenics.MPI.rank(fenics.MPI.comm_world) == 0:
-        debug("abc")
-        info("def")
-        warning("ghi")
-        error("jkl")
-        critical("mno")
         assert not "abc" in caplog.text
         assert "def" in caplog.text
         assert "ghi" in caplog.text
         assert "jkl" in caplog.text
         assert "mno" in caplog.text
-        caplog.clear()
     fenics.MPI.barrier(fenics.MPI.comm_world)
+    caplog.clear()
 
     cashocs.set_log_level(cashocs.LogLevel.WARNING)
+    debug("abc")
+    info("def")
+    warning("ghi")
+    error("jkl")
+    critical("mno")
     if fenics.MPI.rank(fenics.MPI.comm_world) == 0:
-        debug("abc")
-        info("def")
-        warning("ghi")
-        error("jkl")
-        critical("mno")
         assert not "abc" in caplog.text
         assert not "def" in caplog.text
         assert "ghi" in caplog.text
         assert "jkl" in caplog.text
         assert "mno" in caplog.text
-        caplog.clear()
     fenics.MPI.barrier(fenics.MPI.comm_world)
+    caplog.clear()
 
     cashocs.set_log_level(cashocs.LogLevel.ERROR)
+    debug("abc")
+    info("def")
+    warning("ghi")
+    error("jkl")
+    critical("mno")
     if fenics.MPI.rank(fenics.MPI.comm_world) == 0:
-        debug("abc")
-        info("def")
-        warning("ghi")
-        error("jkl")
-        critical("mno")
         assert not "abc" in caplog.text
         assert not "def" in caplog.text
         assert not "ghi" in caplog.text
         assert "jkl" in caplog.text
         assert "mno" in caplog.text
-        caplog.clear()
     fenics.MPI.barrier(fenics.MPI.comm_world)
+    caplog.clear()
 
     cashocs.set_log_level(cashocs.LogLevel.CRITICAL)
+    debug("abc")
+    info("def")
+    warning("ghi")
+    error("jkl")
+    critical("mno")
     if fenics.MPI.rank(fenics.MPI.comm_world) == 0:
-        debug("abc")
-        info("def")
-        warning("ghi")
-        error("jkl")
-        critical("mno")
         assert not "abc" in caplog.text
         assert not "def" in caplog.text
         assert not "ghi" in caplog.text
         assert not "jkl" in caplog.text
         assert "mno" in caplog.text
-        caplog.clear()
     fenics.MPI.barrier(fenics.MPI.comm_world)
+    caplog.clear()
