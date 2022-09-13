9c6b2c98c95c95690d1ee2f2f7b27e699bcabc2c
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2022-09-13 09:00:05 +0200

    Update the tests

--- cashocs/io/convert.py
+++ cashocs/io/convert.py
@@ -36,8 +36,8 @@ def convert(input_file: str, output_file: Optional[str] = None) -> None:
     """
     if fenics.MPI.rank(fenics.MPI.comm_world) == 0:
         if output_file is None:
-            input_name = input_file.rsplit(".", 1)[0]
-            output_file = f"{input_name}.xdmf"
+            cli_convert([input_file])
+        else:
+            cli_convert([input_file, "-o", output_file])
 
-        cli_convert([input_file, output_file])
     fenics.MPI.barrier(fenics.MPI.comm_world)
