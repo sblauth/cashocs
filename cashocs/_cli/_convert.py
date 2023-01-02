1086fb83299c6d78ee46d7d8ce988c9e015f328a
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-01-02 08:08:30 +0100

    New Year's Update - update copyright notice

--- cashocs/_cli/_convert.py
+++ cashocs/_cli/_convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2020-2022 Sebastian Blauth
+# Copyright (C) 2020-2023 Sebastian Blauth
 #
 # This file is part of cashocs.
 #
--- cashocs/_database/geometry_database.py
+++ cashocs/_database/geometry_database.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2022 Sebastian Blauth
+# Copyright (C) 2020-2023 Sebastian Blauth
 #
 # This file is part of cashocs.
 #
