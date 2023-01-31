13ae36e96e595e5aac9cf0160bfb4c44e52cdad3
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-01-31 08:44:43 +0100

    Fix docstrings

--- cashocs/_optimization/optimization_problem.py
+++ cashocs/_optimization/optimization_problem.py
@@ -495,12 +495,19 @@ class OptimizationProblem(abc.ABC):
         Notes:
             If either ``rtol`` or ``atol`` are specified as arguments to the ``.solve``
             call, the termination criterion changes to:
+
             - a purely relative one (if only ``rtol`` is specified), i.e.,
+
             .. math:: || \nabla J(u_k) || \leq \texttt{rtol} || \nabla J(u_0) ||.
+
             - a purely absolute one (if only ``atol`` is specified), i.e.,
+
             .. math:: || \nabla J(u_K) || \leq \texttt{atol}.
+
             - a combined one if both ``rtol`` and ``atol`` are specified, i.e.,
+
             .. math::
+
                 || \nabla J(u_k) || \leq \texttt{atol} + \texttt{rtol}
                 || \nabla J(u_0) ||
 
