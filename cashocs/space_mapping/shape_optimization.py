f4ec570e525ec14e90c08d1c623b89d81e7b99bc
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-03-13 17:06:03 +0100

    Add preconditioner forms to space mapping

--- cashocs/space_mapping/shape_optimization.py
+++ cashocs/space_mapping/shape_optimization.py
@@ -97,6 +97,7 @@ class CoarseModel:
             Union[_typing.KspOption, List[_typing.KspOption]]
         ] = None,
         desired_weights: Optional[List[float]] = None,
+        preconditioner_forms: Optional[List[ufl.Form]] = None,
     ):
         """Initializes self.
 
@@ -118,6 +119,9 @@ class CoarseModel:
             ksp_options: The list of PETSc options for the state equations
             adjoint_ksp_options: The list of PETSc options for the adjoint equations
             desired_weights: The desired weights for the cost functional
+            preconditioner_forms: The list of forms for the preconditioner. The default
+                is `None`, so that the preconditioner matrix is the same as the system
+                matrix.
 
         """
         self.state_forms = state_forms
@@ -132,6 +136,7 @@ class CoarseModel:
         self.ksp_options = ksp_options
         self.adjoint_ksp_options = adjoint_ksp_options
         self.desired_weights = desired_weights
+        self.preconditioner_forms = preconditioner_forms
 
         self._pre_callback: Optional[Callable] = None
         self._post_callback: Optional[Callable] = None
@@ -152,6 +157,7 @@ class CoarseModel:
             ksp_options=self.ksp_options,
             adjoint_ksp_options=self.adjoint_ksp_options,
             desired_weights=self.desired_weights,
+            preconditioner_forms=self.preconditioner_forms,
         )
 
         self.coordinates_optimal = self.mesh.coordinates().copy()
@@ -241,6 +247,7 @@ class ParameterExtraction:
         self.adjoint_ksp_options = (
             coarse_model.shape_optimization_problem.adjoint_ksp_options
         )
+        self.preconditioner_forms = coarse_model.preconditioner_forms
 
         self.coordinates_initial = coarse_model.coordinates_initial
 
@@ -279,6 +286,7 @@ class ParameterExtraction:
             ksp_options=self.ksp_options,
             adjoint_ksp_options=self.adjoint_ksp_options,
             desired_weights=self.desired_weights,
+            preconditioner_forms=self.preconditioner_forms,
         )
         if self.shape_optimization_problem is not None:
             self.shape_optimization_problem.inject_pre_post_callback(
