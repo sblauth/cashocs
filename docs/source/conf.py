b0d832ed2c26c369ccadd3e5dcd905c97d904761
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-03-27 12:17:51 +0200

    Change version to dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = "2020-2023, Sebastian Blauth"
 author = "Sebastian Blauth"
 
 # The full version, including alpha/beta/rc tags
-release = "v2.0.0-alpha3"
+release = "2.0.0-dev"
 
 
 # -- General configuration ---------------------------------------------------
