b249b7b7f7b35a2f8ef6f6601e592aac8fc573a5
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-06-06 09:46:33 +0200

    version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = "2020-2023, Sebastian Blauth"
 author = "Sebastian Blauth"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.1"
+release = "2.0.2"
 
 
 # -- General configuration ---------------------------------------------------
