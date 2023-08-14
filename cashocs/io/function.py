08d0b701574d1f9788c20000847af9a25b502e4c
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-08-14 11:39:45 +0200

    Add function `cashocs.io.import_function`
    This function is similar to `read_function_from_xdmf` but requires a function space.
    The benefit: We can import mesh and function simultaneously.

--- cashocs/io/function.py
+++ cashocs/io/function.py
@@ -19,9 +19,14 @@
 
 from __future__ import annotations
 
+from typing import Optional, TYPE_CHECKING
+
 import fenics
 
 from cashocs.io import mesh as mesh_io
+
+if TYPE_CHECKING:
+    from mpi4py import MPI
 
 
 def read_function_from_xdmf(
@@ -31,6 +36,7 @@ def read_function_from_xdmf(
     degree: int,
     vector_dim: int = 0,
     step: int = 0,
+    comm: Optional[MPI.Comm] = None,
 ) -> fenics.Function:
     """Reads a function from a .xdmf file containing a checkpointed function.
 
@@ -42,12 +48,17 @@ def read_function_from_xdmf(
         vector_dim: The dimension of the vector, if the function is vector-valued. In
             case that this is ``0``, a scalar finite element is assumed. Default is 0.
         step: The checkpoint number. Default is ``0``.
+        comm: The MPI communicator that shall be used. Default is `None`, which means
+            that `fenics.MPI.comm_world` is used.
 
     Returns:
         A fenics representation of the function stored in the file.
 
     """
-    mesh = mesh_io.read_mesh_from_xdmf(filename, step)
+    if comm is None:
+        comm = fenics.MPI.comm_world
+
+    mesh = mesh_io.read_mesh_from_xdmf(filename, step, comm)
     if vector_dim == 0:
         function_space = fenics.FunctionSpace(mesh, family, degree)
     else:
@@ -56,7 +67,33 @@ def read_function_from_xdmf(
         )
 
     function = fenics.Function(function_space)
-    with fenics.XDMFFile(fenics.MPI.comm_world, filename) as file:
+    with fenics.XDMFFile(comm, filename) as file:
+        file.read_checkpoint(function, name, step)
+
+    return function
+
+
+def import_function(
+    filename: str, name: str, function_space: fenics.FunctionSpace, step: int = 0
+) -> fenics.Function:
+    """Imports a function from an xdmf file to cashocs.
+
+    Args:
+        filename: The path / filename to the xdmf file.
+        name: The name of the function in the xdmf file.
+        function_space: The corresponding function space for the function. Note that
+            this must be created on a suitable mesh, which has been reimported with
+            :py:func:`cashocs.io.reimport_mesh`.
+        step: The checkpoint number. Default is ``0``.
+
+    Returns:
+        The fenics function which is stored in the xdmf file.
+
+    """
+    function = fenics.Function(function_space)
+    comm = function_space.mesh().mpi_comm()
+
+    with fenics.XDMFFile(comm, filename) as file:
         file.read_checkpoint(function, name, step)
 
     return function
