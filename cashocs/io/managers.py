7759fc773440210a47b96666d37611dbf9d333ea
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2023-10-25 14:47:19 +0200

    Fixes a bug where the mesh files were not written unless xdmf files were saved.

--- cashocs/io/managers.py
+++ cashocs/io/managers.py
@@ -333,6 +333,28 @@ class TempFileManager(IOManager):
 class MeshManager(IOManager):
     """Manages the output of meshes."""
 
+    def __init__(self, db: database.Database, result_dir: str) -> None:
+        """Initializes the MeshManager class.
+
+        Args:
+            db: The database of the problem.
+            result_dir: The folder where the results are saved.
+
+        """
+        super().__init__(db, result_dir)
+        self.save_mesh = self.config.getboolean("Output", "save_mesh")
+
+    def output(self) -> None:
+        """Saves the mesh as checkpoint for each iteration."""
+        if self.save_mesh:
+            iteration = int(self.db.parameter_db.optimization_state["iteration"])
+
+            iomesh.write_out_mesh(
+                self.db.geometry_db.mesh,
+                self.db.parameter_db.gmsh_file_path,
+                f"{self.result_dir}/checkpoints/mesh/mesh_{iteration}.msh",
+            )
+
     def post_process(self) -> None:
         """Saves a copy of the optimized mesh in Gmsh format."""
         if self.db.parameter_db.problem_type == "shape":
@@ -364,9 +386,7 @@ class XDMFFileManager(IOManager):
         self.save_state = self.config.getboolean("Output", "save_state")
         self.save_adjoint = self.config.getboolean("Output", "save_adjoint")
         self.save_gradient = self.config.getboolean("Output", "save_gradient")
-        self.save_mesh = self.config.getboolean("Output", "save_mesh")
 
-        self.has_output = self.save_state or self.save_adjoint or self.save_gradient
         self.is_initialized = False
 
         self.state_xdmf_list: List[Union[str, List[str]]] = []
@@ -538,20 +558,6 @@ class XDMFFileManager(IOManager):
                     iteration,
                 )
 
-    def _save_mesh(self, iteration: int) -> None:
-        """Saves the mesh as .xdmf and Gmsh .msh file.
-
-        Args:
-            iteration: The current iteration count.
-
-        """
-        if self.save_mesh:
-            iomesh.write_out_mesh(
-                self.db.geometry_db.mesh,
-                self.db.parameter_db.gmsh_file_path,
-                f"{self.result_dir}/checkpoints/mesh/mesh_{iteration}.msh",
-            )
-
     def _write_xdmf_step(
         self,
         filename: str,
@@ -616,4 +622,3 @@ class XDMFFileManager(IOManager):
         self._save_controls(iteration)
         self._save_adjoints(iteration)
         self._save_gradients(iteration)
-        self._save_mesh(iteration)
