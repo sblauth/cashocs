e4704a570928bbfe4e21c7643aefe2d2f18dae55
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2024-03-11 16:00:58 +0100

    import future annotations to prevent py3.8 test failure

--- tests/test_mesh_quality_constraints.py
+++ tests/test_mesh_quality_constraints.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with cashocs.  If not, see <https://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 import pathlib
 
 from fenics import *
