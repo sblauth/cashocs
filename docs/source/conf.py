d0d6c24c3f5255d2095065094c02b959a5384d97
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2025-01-24 08:48:01 +0100

    bump version to 2.5.0-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = "2020-2025, Fraunhofer ITWM and Sebastian Blauth"
 author = "Sebastian Blauth"
 
 # The full version, including alpha/beta/rc tags
-release = "2.4.0"
+release = "2.5.0-dev"
 
 
 # -- General configuration ---------------------------------------------------
