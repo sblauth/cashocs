dd45a87a6371435dc853d6e85343a008d1c9c7e9
Sebastian Blauth <sebastian.blauth@itwm.fraunhofer.de>
2026-01-05 14:22:29 +0100

    version bump to 2.8.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ copyright = "2020-2026, Fraunhofer ITWM and Sebastian Blauth"
 author = "Sebastian Blauth"
 
 # The full version, including alpha/beta/rc tags
-release = "2.8.0-dev"
+release = "2.8.0"
 
 
 # -- General configuration ---------------------------------------------------
